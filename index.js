1e2f141edbe60ea687ebd988eb878289b12b3f62
HumbleNarcissus <maciek.tarach06@gmail.com>
2018-06-21 16:30:23 +0200

    integration with express

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const Discord = require('discord.js');
 const axios = require('axios');
+const express = require('express');
 
+//create express app
+const app = express();
 //create bot
 const bot = new Discord.Client();
+//port
+const port = process.env.PORT || 3000;
 
 //login bot
-require("dotenv").config();
-bot.login(process.env.TOKEN);
+app.listen(port, () => {
+    console.log("Bot is ready to work!");
+    require("dotenv").config();
+    bot.login(process.env.TOKEN);
 
-bot.on('message', message => {
-    //store recent message content
-    const content = message.content;
+    bot.on('message', message => {
+        //store recent message content
+        const content = message.content;
 
-    //react on commands
-    if (content === '!ping') {
-        message.channel.send('pong');
-    } else if (content === '!chuck') {
-        chuck(message);
-    } else if (content === '!yomama') {
-        yomama(message);
-    } else if (message.content === '!help') {
-        message.channel.send("Commands: \n> !chuck\n> !yomama");
-    }
+        //react on commands
+        if (content === '!ping') {
+            message.channel.send('pong');
+        } else if (content === '!chuck') {
+            chuck(message);
+        } else if (content === '!yomama') {
+            yomama(message);
+        } else if (message.content === '!help') {
+            message.channel.send("Commands: \n> !chuck\n> !yomama");
+        }
+    })
 });
 
 //send norris joke
